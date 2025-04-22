fa894153f4a2729bd11409dec7eaff2b79225e38
Albin Baby <albinbabayme05@gmail.com>
2025-04-22 15:34:06 +0200

    changes maded

--- Voting_System/Candidate.cs
+++ Voting_System/Candidate.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Voting_System
 {
-    internal class Candidate
+    public class Candidate
     {
-    }
+        private Guid CandidateId;
+        public string Name;
+        public string Party;
+        private int VoteCount;
+
+        public Candidate(string name, string CandId, string party )
+        {
+            Name = name;
+            CandidateId = Guid.NewGuid();
+            Party = party;
+            VoteCount = 0;
+        }
+
+        public int GetVotecount() => VoteCount;
+        public Guid GetCandidateID() => CandidateId;
+        public void SetVoteCount() { VoteCount += 1;  }
+
+    } 
 }
--- Voting_System/Voter.cs
+++ Voting_System/Voter.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Voting_System
 {
-    internal class Voter
+    internal class Voter : User
     {
+        private bool hasVoted;
+        private Guid VoterId;
+        public Voter(string voterName, string userPassword) : base(voterName, userPassword)
+        {
+            VoterId = Guid.NewGuid();
+            hasVoted = false;
+        }
+
+        public Guid GetVoterId() => VoterId;
+        public bool GetHasVoted() => hasVoted;
+        
+
+        public bool Vote(string candidateId)
+        {
+            if (hasVoted)
+            {
+                Console.WriteLine("You already voted");
+                return hasVoted = false;
+            }
+            Console.WriteLine($"voted successfully to {candidateId}");
+            
+            return hasVoted = true;
+        }
+
+        public override void AccessPortal()
+        {
+            Console.WriteLine($"Welcome, Voter {VoterId}! Accessing voting screen...");
+        }
     }
 }
